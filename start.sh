0e1761dba1bdbe0605fe4c9c2af3f5df9033f1a6
Jannes Magnusson <j-magnusson@t-online.de>
2021-09-30 10:28:48 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-gnome-terminal --tab --title=PHP -- bash -c "gnome-terminal --tab --title=NODE -- bash -c \"cd webpage; npm start\"; cd server; php -S localhost:8080 -t ."
+gnome-terminal --tab --title=PHP -- bash -c \\
+    "gnome-terminal --tab --title=NODE -- bash -c \"cd webpage; npm install; npm start\"; cd server; php -S localhost:8080 -t ."
