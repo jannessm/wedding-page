2c605efa66dd3b63b955dc5c38318dfd04d62558
Jannes Magnusson <jannes.hp@t-online.de>
2021-10-18 17:20:10 +0200

    Update budget.php

--- server/api/admin/budget.php
+++ server/api/admin/budget.php
@@ -59,3 +59,43 @@ function deleteCategory() {
 
     respondJSON(201, "deleted category");
 }
+
+function updateCostCenters() {
+    global $BASE;
+    $payload = json_decode(file_get_contents("php://input"), true);
+    $data = json_decode(read_file($BASE . 'budget-data'), true);
+
+    $data['categories'] = $payload['categories'];
+    $data['cost_centers'] = $payload['costCenters'];
+
+    write_file($BASE . 'budget-data', json_encode($data));
+
+    respondJSON(201, "updated categories");
+}
+
+function deleteCostCenter() {
+    global $BASE;
+    $cost_center_id = $_GET['id'];
+    $data = json_decode(read_file($BASE . 'budget-data'), true);
+
+    $categories = $data['categories'];
+    foreach($categories as $c) {
+        $categories['cost_center_ids'] = array_filter(
+            $categories['cost_center_ids'],
+            function ($id) use ($cost_center_id) {
+                return strcmp($id, $cost_center_id) >= 0;
+            }
+        );
+    }
+    
+    $data['cost_centers'] = array_filter(
+        $categories['cost_centers'],
+        function ($cc) use ($cost_center_id) {
+            return strcmp($cc['id'], $cost_center_id) < 0;
+        }
+    );
+
+    write_file($BASE . 'budget-data', json_encode($data));
+
+    respondJSON(201, "updated categories");
+}
